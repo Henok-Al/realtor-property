d1ad282826450ae73406748867c78f5923074357
Henok-Al <henok.gebresenbet@gmail.com>
2024-05-27 18:36:05 +0300

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Footer from "@/components/Foooter";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="text-blue-300"><h1 className='text-3xl'>Welcome</h1>
-    <Link href='/properties'>Properties</Link></div>
+    <div className="text-blue-300">
+      <Hero />
+      <InfoBoxes />
+      <Footer />
+
+      {/* <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Properties</Link> */}
+    </div>
   );
 }
