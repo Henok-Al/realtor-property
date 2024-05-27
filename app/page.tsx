47a998b0cba15c80a5f7f0d4252953d192d00664
Henok-Al <henok.gebresenbet@gmail.com>
2024-05-27 19:19:53 +0300

    render properties on homepage and limit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Footer from "@/components/Foooter";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import Image from "next/image";
-import Link from "next/link";
+import HomeProperties from "@/components/HomeProperties";
 
 export default function HomePage() {
   return (
     <div className="text-blue-300">
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
       <Footer />
-
-      {/* <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Properties</Link> */}
     </div>
   );
 }
